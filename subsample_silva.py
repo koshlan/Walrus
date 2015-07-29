85499113ddfcb59646af4c2faef4f8dda0d09fde
Koshlan <koshlan@gmail.com>
2015-07-29 15:03:38 -0700

    Update subsample_silva.py

--- subsample_silva.py
+++ subsample_silva.py
@@ -34,17 +34,40 @@ SILVA_123_SSURef_Nr99_tax_silva.fasta and reduced it to many fewer \
 sequences that include coverage of all phylum and lower orders')
 parser.add_argument('-in', action="store", dest="fasta_filename")
 parser.add_argument('-min_length', action="store", dest="min_length_threshold", default=1450)
-parser.add_argument('-min_taxa_depth', action="store", dest="min_taxa_depth", default=5)
+parser.add_argument('-max_taxa_depth', action="store", dest="max_taxa_depth", default=5)
 r = parser.parse_args()
 
+# This block opens the fasta file
 from Bio import SeqIO
 import sys
 handle = open(r.fasta_filename, "rU")
 
+
+# walk
+
+# Args:
+    # l is a list 
+# Returns
+    # a tupel (the first item in the list and and abreviated list)
 def walk(l):
     item = l.pop(0)
     return(item, l)
-    
+
+# is_novel 
+
+# This helper function is recursive. It looks at a list and a reference. 
+# If the list is greater than 1 in length, it takes a step down the list,
+# if the item is novel it returns true; whereas if the item is already in 
+# the reference list, it takes another step. The maximum number of steps are specified
+# by the length of the list supplied. The user controls this by -min_taxa_depth flag
+
+# Args:
+    # L is a list of strings
+    # ref is a list of refernce strings 
+# Reurns
+    # a tuple: (Boolean, String)
+        # True if the function finds a novel string, and the string 
+        # False if the function does not find a novel string and a "Null" string
 def is_novel(L, ref):
     if len(L) <= 1:
         return False, "Null"
@@ -54,27 +77,24 @@ def is_novel(L, ref):
             return True, string
         else:
             return(is_novel(L,ref))
-ref = []
-count = 0
+
+ref = [] # This is the blank reference list that will grow as novel taxa are discovered
 min_length_threshold = r.min_length_threshold#1450
 for record in SeqIO.parse(handle, "fasta") :
-    count = count + 1
+    # This rejectes sequences that are not long enough (user specifies min with -min_length flag
     if len(record.seq) > min_length_threshold: 
-        #print record.description.split(" ")[1]
+        #This rejects sequences from uncultured organisms
         if "uncultured" in record.description.split(" ")[1].split(";"):
             continue
-        genus_tree = record.description.split(" ")[1].split(";")[0:r.min_taxa_depth]
+        genus_tree = record.description.split(" ")[1].split(";")[0:r.max_taxa_depth]
+        #This rejects sequences Eukaryota Kingdom
         if genus_tree[0] == "Eukaryota":
             continue
-            
+        # This will return a tuple: (Boolean, String)
         a = is_novel(genus_tree, ref)
-        #print a
         if a[0]:
             ref.append(a[1])
-            #sys.stdout.write(str(record.description.split(" ")[1].split(";")) + "\n")
             sys.stdout.write(">%s\n%s\n" %(record.description, record.seq))
         else:
             continue
-    #if count > 10000:
-    #    break
 handle.close()
