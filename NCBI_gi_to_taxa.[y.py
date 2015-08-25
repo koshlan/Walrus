9992e9b5a87fcfc00d7734322bc256a126480906
Koshlan <koshlan@gmail.com>
2015-08-25 11:08:35 -0700

    Update

--- NCBI_gi_to_taxa.[y.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'koshlan'
-with open(...) as f:
-    for line in f:
-        line.strip().split("\t")
--- join.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('taxa.db')
-c = conn.cursor()
-# LIST TABLES
-c.execute('''SELECT toy.id my_var1 FROM toy JOIN taxa ON toy.id = taxa.id''')
-# close connection
-conn.close()
-
